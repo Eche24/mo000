f1de70df85445478b3723e4144642a52e3939937
eche ngbede <spwesn@gmail.com>
2022-04-17 18:14:39 -0500

    Update kubesetup.sh

--- kubesetup.sh
+++ kubesetup.sh
@@ -6,3 +6,4 @@ cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+#master node only
